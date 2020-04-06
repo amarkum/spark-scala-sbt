b5b0f8e27150cceb35d8d78a41f0055ca95cae96
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-06 18:28:56 +0530

    Added MultiSpark Session Example

---
+++ src/main/scala/com/spark/scala/sbt/MultiSparkSession.scala
@@ -0,0 +1,28 @@
+package com.spark.scala.sbt
+
+import org.apache.spark.sql.SparkSession
+
+object MultiSparkSession {
+
+  def main(args: Array[String]): Unit = {
+
+    // Create 2 SparkSession
+    val sparkSessionOne = SparkSession.builder()
+      .master("local")
+      .appName("Spark Session One")
+      .getOrCreate()
+
+    val sparkSessionTwo = SparkSession.builder()
+      .master("local")
+      .appName("Spark Session Two")
+      .getOrCreate()
+
+    // Create two RDD with different SparkSession
+    val rddOne = sparkSessionOne.sparkContext.parallelize(Array(1, 2, 3, 4, 5))
+    val rddTwo = sparkSessionTwo.sparkContext.parallelize(Array(6, 7, 8, 9, 10))
+
+    // Print the value
+    rddOne.collect().foreach(println)
+    rddTwo.collect().foreach(println)
+  }
+}
