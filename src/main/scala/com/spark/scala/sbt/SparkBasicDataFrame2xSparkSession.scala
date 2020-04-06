a6683ab8128f3a4b790c4dfdde49a75c5591087a
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-06 12:35:22 +0530

    Added code to create df in Spark 2.X

---
+++ src/main/scala/com/spark/scala/sbt/SparkBasicDataFrame2xSparkSession.scala
@@ -0,0 +1,36 @@
+package com.spark.scala.sbt
+
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+
+/**
+ * Spark 2.X
+ */
+object SparkBasicDataFrame2xSparkSession {
+
+  // Create a Spark Session, object is returned by Builder class
+  val sparkSession = SparkSession.builder()
+    .master("local")
+    .appName("Spark 2 DataFrame")
+    .getOrCreate()
+
+  // Create the RDD by utilizing SparkContext of SparkSession
+  val rdd = sparkSession.sparkContext.parallelize(Array(1,2,3,4,5))
+
+  // Create an Schema
+  val schema = StructType(
+    StructField("Numbers", IntegerType,false):: Nil
+  )
+
+  // Create a specific Row RDD from RDD
+  val rowRdd = rdd.map(line => Row(line))
+
+  // Create a df from createDataFrame of sparkSession
+  val df = sparkSession.createDataFrame(rowRdd,schema)
+
+  //Show the schema
+  df.printSchema()
+
+  //show the content of df
+  df.show()
+}
