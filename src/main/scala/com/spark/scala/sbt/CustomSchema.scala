ac363b9066099393c5b108caac4a4ae937a884cb
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-07 16:09:32 +0530

    Added code for Custom Schema

---
+++ src/main/scala/com/spark/scala/sbt/CustomSchema.scala
@@ -0,0 +1,39 @@
+package com.spark.scala.sbt
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+
+object CustomSchema {
+
+  def main(args : Array[String]): Unit ={
+
+    val sparkSession = SparkSession.builder()
+      .master("local")
+      .appName("Custom Schema for Data Frame")
+      .getOrCreate()
+
+    // Create a Custom Schema
+    val customSchema = StructType(
+      StructField("Location", StringType, true)
+        :: StructField("City", StringType, true)
+        :: StructField("Organization", StringType, true)
+        :: StructField("FirstName", StringType, true)
+        :: StructField("LastName", StringType, true)
+        :: StructField("Country", StringType, true)
+        :: StructField("Reseller", StringType, true)
+        :: StructField("State", StringType, true)
+        :: StructField("Zipcode", IntegerType, true)
+        :: Nil
+    )
+
+    //Impose Custom Schema to Data Frame
+      val dataFrame = sparkSession.read
+        .schema(customSchema)
+        .options(Map("header"->"true"))
+        .csv("src/main/resources/csv/*")
+
+    dataFrame.printSchema()
+    dataFrame.show()
+  }
+
+}
