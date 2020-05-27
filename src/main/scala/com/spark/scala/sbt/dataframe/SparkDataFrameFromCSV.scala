f2042fc57ac51b0844df40c739e0cbba355b2306
Amar Kumar <amar.mailsbox@gmail.com>
2020-05-28 00:15:51 +0530

    Updated Spark Code

--- src/main/scala/com/spark/scala/sbt/dataframe/SparkDataFrameFromCSV.scala
+++ src/main/scala/com/spark/scala/sbt/dataframe/SparkDataFrameFromCSV.scala
@@ -12,10 +12,34 @@ object SparkDataFrameFromCSV {
       .appName("Spark DF from CSV")
       .getOrCreate()
 
+    import sparkSession.implicits._
+
     val df = sparkSession.read.options(Map("header" -> "true", "inferSchema" -> "true"))
       .csv("src/main/resources/dataframe/csv/customer.csv")
 
-    df.printSchema()
-    df.show()
+    // To use "$" in-front of a column name, as evaluation operator we must import implicits
+
+    // select will select only the required columns
+    val selectedDF = df.select("Address", "Reseller")
+
+    // select will select only the required columns, $ is mandatory for all columns.
+    val filteredDF = selectedDF.select($"Address", ($"Reseller"+3).as("Reseller"))
+
+    filteredDF.printSchema()
+    filteredDF.show()
+
+    // filter using $ | using select will fill in with the expression check.
+    val limitedDF = filteredDF.filter($"Reseller"<4)
+
+    // createOrReplaceTempView is used to create a SQL kind of table
+    limitedDF.createOrReplaceTempView("resller")
+
+    val sqlDF = sparkSession.sql("SELECT * FROM resller")
+
+    // Register the DataFrame as a global temporary view
+    df.createGlobalTempView("resell")
+
+    // Global temporary view is tied to a system preserved database `global_temp`sqlDF.show()
+    sparkSession.sql("SELECT * FROM global_temp.resell").show()
   }
 }
